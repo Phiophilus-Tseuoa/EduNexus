02b197824a797a8b4e65e8a882b919a91dd3112a
Phiophilus Tseuoa <siyabongamzizi@icloud.com>
2025-09-20 13:10:08 +0200

    Initial MAUI project setup

---
+++ EduNexusApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EduNexusApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EduNexusApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EduNexusApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "EduNexusApp.Pages")]
